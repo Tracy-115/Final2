6ba864eaf9e5a8231035bdf515cd39d4994313e6
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-11 19:35:24 -0800

    added the queue for the customer

--- final2.cpp
+++ final2.cpp
@@ -7,5 +7,25 @@ struct Node { //lab-11
   Node* next;
 };
 int main(){
+    string names[5] = {"John", "Jane", "Joe", "Jill", "Jack"};
+    string drinks[5] = {"Coke", "Pepsi", "Sprite", "Fanta", "Dr Pepper"};
+    Node* head = nullptr;
+    Node* tail = nullptr;
 
+    for (int i = 0; i<3; i++){
+        Node* newNode = new Node;
+        newNode -> name = names [rand() % 10];
+        newNode -> drink = drinks [rand90 % 8];
+        newNode -> next = nullptr;
+
+        if (head == nullptr){
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail -> next = newNode;
+            tail = newNode;
+        }
+    }
+    
 }
